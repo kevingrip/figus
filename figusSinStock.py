139eec84d581d5379c3755e032d4a501e2847417
Kevin Grippo <grippokevin@gmail.com>
2023-11-06 07:19:28 -0300

    act

--- figusSinStock.py
+++ figusSinStock.py
@@ -9,7 +9,7 @@ faltantes = []
 cantidad=0
 
 for figurita in baseTotal:
-    if figurita["CANT"] in (0,1) and figurita["PRECIO"] <800:
+    if figurita["CANT"] == (2) and figurita["PRECIO"] <800:
         num = figurita["NUM"]
         cant = figurita["CANT"]
         precio = figurita["PRECIO"]
