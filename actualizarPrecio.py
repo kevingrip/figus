3eab845d695d03752236d8c00b558319ff13e88c
Kevin Grippo <grippokevin@gmail.com>
2023-11-30 02:08:25 -0300

    act

--- actualizarPrecio.py
+++ actualizarPrecio.py
@@ -4,12 +4,12 @@ with open ("baseMundial.json","r") as baseJson:
     baseMundial = json.load(baseJson)
 
 for linea in baseMundial:
-    if linea["NUM"][:3]=="ARG":
-        if linea["PRECIO"]==600:
-            linea["PRECIO"]=850
+    if linea["NUM"][:3]=="FWC":
+        if linea["PRECIO"]==1500:
+            linea["PRECIO"]=1600
 
 for linea in baseMundial:
-    if linea["NUM"][:3]=="ARG":
+    if linea["NUM"][:3]=="FWC":
         print(linea)
 
 with open ("baseMundial.json","w") as baseEditarJson:
