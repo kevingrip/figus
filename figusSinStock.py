8f17c878e1a2b766dd9b2784a99559b5f916aa95
Kevin Grippo <grippokevin@gmail.com>
2024-05-02 02:08:45 -0300

    act

--- figusSinStock.py
+++ figusSinStock.py
@@ -13,7 +13,7 @@ faltantes = []
 cantidad=0
 
 for figurita in baseTotal:
-    if figurita["CANT"] == (1):
+    if figurita["CANT"] in (0,1):
         num = figurita["NUM"]
         cant = figurita["CANT"]
         precio = figurita["PRECIO"]
