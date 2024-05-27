ee30636f296a2cb7ef8ff7b77bcc9ef8e3afe3dd
Kevin Grippo <grippokevin@gmail.com>
2024-05-27 13:00:03 -0300

    act

--- actualizarPrecio.py
+++ actualizarPrecio.py
@@ -2,14 +2,14 @@ import json
 
 from bases import*
 
-base = baseCopamerica()
+base = baseMundial()
 
 for linea in base:
-    if linea["TIPO"]=='ESCUDO' and linea["PRECIO"]==1500:
-        linea["PRECIO"]=2000
+    if linea["PRECIO"]==1500:
+        linea["PRECIO"]=2500
 
 for linea in base:
     print(linea)
 
-with open ("base_copam.json","w") as baseEditarJson:
+with open ("baseMundial.json","w") as baseEditarJson:
     json.dump (base,baseEditarJson,indent=4)
