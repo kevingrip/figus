260d61f52d5e222366f374d548a26975c631b8c2
Kevin Grippo <grippokevin@gmail.com>
2024-06-05 00:23:40 -0300

    act

---
+++ sacarFiguCopam.py
@@ -0,0 +1,18 @@
+from bases import *
+import json
+
+bm = baseCopamerica()
+
+cant=0
+
+for figu in bm:
+    if (figu["CANT"]>7):
+        if figu["PRECIO"]==850:
+            cant+=figu["CANT"]-7
+            figu["CANT"] = figu["CANT"] - (figu["CANT"]-7)
+            print(figu)
+
+print(cant)
+
+with open ('base_copam.json','w') as bcm:
+    json.dump(bm,bcm,indent=4)
