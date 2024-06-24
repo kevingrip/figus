b8dfc8fcd41dcf7a031e42623705d948b53a81a0
Kevin Grippo <grippokevin@gmail.com>
2024-06-24 17:54:31 -0300

    act

--- main.js
+++ main.js
@@ -222,7 +222,7 @@ const totalVentas = () =>{
                     figusNoVendidas.textContent = `${figuNoVend}`
                     figusNoVendidas.style.color = 'red'
 
-                    if (objeto["usuario"].includes('SOYDAZO')||objeto["usuario"].includes('AGMAFAMSRL')||objeto["usuario"].includes('TINCHO2051')||objeto["usuario"].includes('MAUROKOZUR')||objeto["usuario"].includes('NATALIASOLEDADCARACOCHE')||objeto["usuario"].includes('ROFEMAF')||objeto["usuario"].includes('GIMENEZLORENABEATRIZ')){
+                    if (objeto["usuario"].includes('SOYDAZO')||objeto["usuario"].includes('AGMAFAMSRL')||objeto["usuario"].includes('TINCHO2051')||objeto["usuario"].includes('MAUROKOZUR')||objeto["usuario"].includes('NATALIASOLEDADCARACOCHE')||objeto["usuario"].includes('DIEGODANIELTUNINETTI')||objeto["usuario"].includes('DIEFONTE')){
                         objetoDiv.style.backgroundColor='skyblue'
                     }
                     
