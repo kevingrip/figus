9d176fcf84d55817ce8a671bffd54ce4a5a18489
Kevin Grippo <grippokevin@gmail.com>
2024-07-08 00:17:51 -0300

    act

---
+++ mecanismoFinde.py
@@ -0,0 +1,18 @@
+import json
+
+with open ("totalVentas.json","r") as tv:
+    totalVentas = json.load(tv)
+
+totalfigus=[]
+
+cant=0
+
+for x in (totalVentas["Copa America 2024"]):
+    if x["PREPARADO"]=="NO":
+        if x["Dia"]>"07/07/2024":
+            for x in (x["Vendidas"]):            
+                cant+=1
+                totalfigus.append(x)
+
+print(cant)
+print(sorted(totalfigus))
