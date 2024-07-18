321131ebe3a55b300b1abc58fafe331e85c99aab
Kevin Grippo <grippokevin@gmail.com>
2024-07-18 20:41:36 -0300

    act

--- mecanismoMasivo.py
+++ mecanismoMasivo.py
@@ -9,7 +9,7 @@ cant=0
 
 for x in (totalVentas["Copa America 2024"]):
     if x["PREPARADO"]=="NO" and x["usuario"] not in ('NIJUMALA'):
-        if x["Dia"]>"15/07/2024":
+        if x["Dia"]>"16/07/2024":
             for x in (x["Vendidas"]):            
                 cant+=1
                 totalfigus.append(x)
