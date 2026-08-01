f99abf4a1d2d75383dff4945c99d97ec2c51a109
Kevin Grippo <grippokevin@gmail.com>
2026-08-01 20:19:45 -0300

    aplicado meli

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import proveedores from "./routes/proveedores.js"
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import mercadoLibreRoutes from "./routes/rutas_ml.js"
+import mercadoLibreRoutes from "./routes/ruta_orden_ml.js"
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -22,7 +22,7 @@ const app = express()
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public", "pages")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 const PORT = process.env.PORT || 5050;
 
