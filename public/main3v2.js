d64a6ff9514464849c9dd269cc20ae738c5b945e
Kevin Grippo <grippokevin@gmail.com>
2026-08-21 11:14:53 -0300

    Update main3v2.js

--- public/main3v2.js
+++ public/main3v2.js
@@ -268,7 +268,7 @@ const actualizarVentas = async () => {
 
     for (const pregunta of preguntasMDB) {
         if (pregunta.COMPRADO === false) {
-            console.log(pregunta.BUYER_ID)
+            if (pregunta.BUYER_ID === 181068016) {console.log(pregunta.BUYER_ID)}
             for (const venta of ventasML) {
                 if ((venta.data.buyer_id === pregunta.BUYER_ID)
                     && (venta.data.seller === pregunta.SELLER_ID)
