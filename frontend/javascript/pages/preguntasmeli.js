1ad8205645207fe900fa22fad897b1236efbf253
Kevin Grippo <grippokevin@gmail.com>
2026-08-24 14:50:00 -0300

    Update preguntasmeli.js

--- frontend/javascript/pages/preguntasmeli.js
+++ frontend/javascript/pages/preguntasmeli.js
@@ -31,7 +31,6 @@ const responderPregunta = async ({ elementPregunta, idPregunta, valorMensaje, ve
 const cargarPreguntaMDB = async (figusEnStock, figusSinStock, vendedor, cliente, albumConsulta, fecha, albumReal, mla) => {
     try {
         if (figusEnStock){
-            console.log(figusEnStock)
             const preguntambd = await guardarPreguntaML(figusEnStock, figusSinStock, vendedor, cliente, albumConsulta, fecha, albumReal, mla)
         }        
     } catch (error) {
